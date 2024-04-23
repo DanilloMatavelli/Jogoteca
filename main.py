49769b4ec694f31eb837c1729fe939e71ed426d4
Danillo Matavelli <danillomatavelli26@gmail.com>
2024-04-23 08:31:12 -0300

    add: Validação por idade

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ print("TEMOS OS SEGUINTES JOGOS DISPONÍVEIS")
 print("1 - JOGO DA CALCULADORA")
 print("2 - JOGO DA FORCA MORTAL")
 print("3 - JOGO DA VELHA")
-idade_jogador = int(input("Qual sua idade"))
+
+#Validação por idade
+idade_jogador = int(input("Qual sua idade:"))
+if idade_jogador <18:
+    print("Sai daqui muleke, você não é permitido aquii!!")
+else:
+    print("Pode entrar, você está liberado!!!")
+    escolhe_um_jogo = int(input("Qual jogo você deseja jogar?"))
