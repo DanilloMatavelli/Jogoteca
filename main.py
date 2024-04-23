5b3737757f605dc80b2ea9825059593110739240
Danillo Matavelli <danillomatavelli26@gmail.com>
2024-04-23 08:50:28 -0300

    add: Validação de dados

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
+
+os.system("cls")
+
+# Menu Principal
 print("SEJA BEM-VINDO AO JOGOTECA!!!")
 print("DESENVOLVIDO POR DANILLO!!!")
-print("TEMOS OS SEGUINTES JOGOS DISPONÍVEIS")
-print("1 - JOGO DA CALCULADORA")
-print("2 - JOGO DA FORCA MORTAL")
-print("3 - JOGO DA VELHA")
 
 #Validação por idade
 idade_jogador = int(input("Qual sua idade:"))
@@ -11,4 +12,8 @@ if idade_jogador <18:
     print("Sai daqui muleke, você não é permitido aquii!!")
 else:
     print("Pode entrar, você está liberado!!!")
+    print("TEMOS OS SEGUINTES JOGOS DISPONÍVEIS")
+    print("1 - JOGO DA CALCULADORA DA MORTE")
+    print("2 - JOGO DA FORCA MORTAL")
+    print("3 - JOGO DA VELHA ZUMBI")
     escolhe_um_jogo = int(input("Qual jogo você deseja jogar?"))
