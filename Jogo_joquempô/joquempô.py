7cef6281d4834c40f745388a7f9c41a6ddbe06c3
Danillo Rodrigues Matavelli <danillomatavelli26@gmail.com>
2024-04-29 14:29:07 -0300

    add: Modificações Joquempô

--- Jogo_joquempô/joquempô.py
+++ Jogo_joquempô/joquempô.py
@@ -3,16 +3,19 @@ print("BORA COMEÇAR A JOGAR!!!")
 
 import random
 
+papel = 1
+tesoura = 2
+pedra = 3
 #Escolha do usuario
-escolha_o_que_ira_jogar = input("Agora é sua vez de jogar, escolha entre papel , tesoura ou pedra:")
 
-if escolha_o_que_ira_jogar == "papel" :
-    papel = 1 
-if escolha_o_que_ira_jogar == "tesoura":
-    tesoura = 2
-if escolha_o_que_ira_jogar == "pedra":
-    pedra = 3
+escolha_o_que_ira_jogar = input("Agora é sua vez de jogar, escolha entre 1- Papel , 2- Tesoura ou 3- Pedra:").upper()
 
+if escolha_o_que_ira_jogar =="1":
+    escolha_o_que_ira_jogar = "Papel"
+if escolha_o_que_ira_jogar =="2":
+    escolha_o_que_ira_jogar = "Tesoura"
+if escolha_o_que_ira_jogar =="3":
+    escolha_o_que_ira_jogar = "Pedra"
 #Escolha da máquina
 numero_aleatorio = random.randint(1,3)
 if numero_aleatorio == 1:
@@ -23,6 +26,10 @@ if numero_aleatorio == 3:
     numero_aleatorio = "pedra"
 
 print(numero_aleatorio)
+
+#Verificando as combinações 
+
+
 
 
 
